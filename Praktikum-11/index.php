76389b581293ca764bdae4c1ed897fd8dac6dc59
Arsya Apricia <arsyaapriciap@email.com>
2026-05-01 10:33:43 +0700

    upload Praktikum-11

---
+++ Praktikum-11/index.php
@@ -0,0 +1,19 @@
+<?php
+        session_start();
+        if (!isset($_SESSION['login_Un51k4'])) {
+            header("Location: login.php?message=" .
+urlencode("Mengakses fitur harus login dulu bro."));
+            exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello</h1>
+</body>
+</html>
